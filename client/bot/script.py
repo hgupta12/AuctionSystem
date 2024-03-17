784da0ddf8f0d40fddaef2b0c30f24f3f09a803f
csking101 <chinmayasahu101@gmail.com>
2024-03-17 13:02:26 +0530

    Made the rating system proper

--- client/bot/script.py
+++ client/bot/script.py
@@ -4,23 +4,40 @@ import json
 
 players_data = []
 
-def calculate_rating(player) -> float:
+def calculate_rating(player, max_ba, max_bsr) -> float:
     """
     Finds how valuable the player is to the team
     """
     batting_weight = 0.6
     bowling_weight = 0.4
-    batting_rating = (float(player["runs_scored"]) * float(player["batting_average"])) / float(player["ball_faced"]) if float(player["ball_faced"]) > 0 else 0
-    bowling_rating = (float(player["wickets"]) * float(player["bowling_strike_rate"])) / float(player["runs_conceded"]) if float(player["runs_conceded"]) > 0 else 0
-    overall_rating = (batting_weight * batting_rating) + (bowling_weight * bowling_rating)
-    return round(overall_rating, 3)
+    batting_rating = float(player['batting_average'])/max_ba
+    bowling_rating = float(player['bowling_strike_rate'])/max_bsr
+    # overall_rating = (batting_weight * batting_rating) + (bowling_weight * bowling_rating) #In the range 0-1
+    overall_rating = max(batting_rating, bowling_rating)
+    
+    overall_rating *= 10
+    
+    return overall_rating
 
 def csv_to_json(file_path: str) -> None:
     """
     Convert the CSV players data to JSON data
     """
+    #CHANGE THE RATING CALCULATION
+    max_ba = 0 #Batting average
+    max_bsr = 0 #Bowling strike rate
+    
     with open(file_path, 'r') as csv_file:
         reader = csv.DictReader(csv_file)
+        
+        
+        for player in reader:
+            max_ba = max(max_ba, float(player['batting_average']))
+            max_bsr = max(max_bsr, float(player['bowling_strike_rate']))
+        
+    with open(file_path, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        
         for i, player in enumerate(reader):
             role = ""
             if float(player['runs_scored']) > 100:
@@ -34,7 +51,7 @@ def csv_to_json(file_path: str) -> None:
             state = 0
             player_obj = {
                 "index": i + 1,
-                "rating": calculate_rating(player),
+                "rating": calculate_rating(player, max_ba, max_bsr),
                 "role": role,
                 "base_price": base_price,
                 "current_price": current_price,
